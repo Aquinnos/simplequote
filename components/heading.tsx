19b5da6f46893be791069333d1eda63b342450a2
Aquinnos <k.synakowski123@gmail.com>
2025-02-06 09:45:50 +0100

    Remove unused quote styles, update heading text size, and delete favicon.svg

--- components/heading.tsx
+++ components/heading.tsx
@@ -4,7 +4,7 @@ import { ColourfulText } from './ui/colourful-text';
 const Heading = () => {
   return (
     <div className="relative flex justify-center items-center h-20">
-      <h1 className="text-white lg:text-8xl md:text-6xl sm:text-4xl ">
+      <h1 className="text-white lg:text-8xl md:text-8xl sm:text-7xl text-6xl">
         Simple <ColourfulText text="Quote" />
       </h1>
     </div>
