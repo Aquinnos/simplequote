a5ab2c241b2e45c18f15a980210fba4845484006
Krystian Synakowski <117024828+Aquinnos@users.noreply.github.com>
2025-02-13 10:25:09 +0100

    Update route.ts

--- app/api/quote/route.ts
+++ app/api/quote/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     const response = await fetch('https://zenquotes.io/api/random');
     const data = await response.json();
 
-    const quoteText = data[0].q; // Cytat w języku angielskim
+    const quoteText = data[0].q; 
     const quoteAuthor = data[0].a;
 
     return NextResponse.json({ q: quoteText, a: quoteAuthor });
